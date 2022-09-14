4290eca5af8242e8f9b89e7a7d1e659d92d854fa
German Arutyunov <garutyunov@tenderhub.ru>
2022-09-14 04:03:51 +0300

    datadog and gsp removed

--- versions.tf
+++ versions.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    datadog = {
-      source  = "DataDog/datadog"
-      version = ">=3.5.11"
-    }
     helm = {
       source  = "hashicorp/helm"
       version = ">=2.6.0"
